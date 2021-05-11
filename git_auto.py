0c1b71f1d6649bae9532e7f397f808007ed43783
BuildTools <unconfigured@null.spigotmc.org>
2021-05-11 05:19:55 +0200

    Automated full login

--- git_auto.py
+++ git_auto.py
@@ -1,5 +1,36 @@
-import os,sys
+import os,sys,time,pyautogui
 
 path = "C:\Program Files\Git\git-bash.exe"
 os.startfile(path)
 
+def cdFolder(fdirectory):
+    pyautogui.write('cd ' + fdirectory)
+    pyautogui.press('enter')
+    
+def gitPull():
+    pyautogui.write('git pull')
+    pyautogui.press('enter')
+    time.sleep(1.5)
+
+def gitStatus():
+    pyautogui.write('git status')
+    pyautogui.press('enter')
+    time.sleep(1)
+
+def gitAddAll():
+    pyautogui.write('git add .')
+    pyautogui.press('enter')
+    time.sleep(1)
+
+def gitCommit():
+    pyautogui.write('git commit -m "New Commit"')
+    pyautogui.press('enter')
+    time.sleep(1)
+
+if __name__ == "__main__":
+    time.sleep(1)
+    cdFolder('/e/git/Projects')
+    gitPull()
+    gitStatus()
+    gitAddAll()
+    gitCommit()
