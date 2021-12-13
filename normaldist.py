141e567aaa6067348e0b3e9e041ba01a0b2fe300
Alejandro Roigé <49212244+roigecode@users.noreply.github.com>
2021-12-13 04:05:38 +0100

    Empirical Rule Prove + Option's POP

--- normaldist.py
+++ normaldist.py
@@ -1,5 +1,6 @@
 from manim import *
-from math import sqrt, exp, pi
+from math import sqrt, exp, pi, erfc
+from manim.mobject.mobject import T
 from manim.utils import scale
 import csv
 import numpy as np
@@ -27,7 +28,7 @@ class MainFunction(MovingCameraScene):
         self.wait()
         self.play(FadeOut(t0))
 
-        t1 = Tex(r"¿",r"Why when we sell a first standard deviation $(1 \sigma)$ option we have a ",r"68\%",r" POP",r"?", font_size=35)
+        t1 = Tex(r"¿",r"Why when we sell a first standard deviation $(1 \sigma)$ option we have a ",r"68\%",r" POP",r"?", font_size=25)
         t1[2].set_color(PURE_GREEN)
 
         t2 = MathTex(r'\dfrac{e^{-\frac{(kx - \mu)^2}{2 \sigma^2}}}{\sigma \sqrt{2 \pi}}')
@@ -71,7 +72,7 @@ class MainFunction(MovingCameraScene):
         st = MathTex(r'\sigma = 1').move_to(DOWN*0.75)
 
         iex = Tex(r'When we integrate a PDF we get the probability as the area bellow the bounded curve. We will rewrite our expression as:',font_size=25).move_to(DOWN*1.5)
-        iex2 = Tex(r'our limits of integration are -1 \& 1 because we want to know POP at $1\sigma$',font_size=20).move_to(DOWN*1.5)
+        iex2 = Tex(r'Our limits of integration are -1 \& 1 because we want to know POP at $1\sigma$',font_size=25).move_to(DOWN*1.5)
         iex3 = Tex(r'Substitute $u = \dfrac{x}{\sqrt{2}}$ then $\dfrac{du}{dx} = \dfrac{1}{\sqrt{2}} \, \therefore\, dx = \sqrt{2}\, du$:', font_size=25).move_to(UP*1.5)
         iex4 = Tex(r'Undo the substitution $u = \dfrac{x}{\sqrt{2}}$:',font_size=30).move_to(DOWN*2)
         iex5 = Tex(r'Now we have $\int f(x)\, dx = F(x)$ so we need to evaluate it between our boundaries to get $\int_{-1}^{1} f(x)\, dx$:',font_size=30).move_to(DOWN*2)
@@ -93,7 +94,7 @@ class MainFunction(MovingCameraScene):
         self.play(t1.animate.move_to(UP*2.5))
 
         # Re-arrenge text:
-        self.play(FadeOut(t1[1]), FadeOut(t1[3]), t1[2].animate.move_to(center_dot.get_center()), t1[0].animate.shift(RIGHT*5.5),t1[4].animate.shift(LEFT*5.5))
+        self.play(FadeOut(t1[1]), FadeOut(t1[3]), t1[2].animate.move_to(center_dot.get_center()), t1[0].animate.shift(RIGHT*4),t1[4].animate.shift(LEFT*4))
 
         self.play(Write(ax))
         self.play(Write(curve), Write(mu), Write(sigma1), Write(sigma2), Write(sigma3), Write(sigma4), Write(sigma5), Write(sigma6))
@@ -115,7 +116,7 @@ class MainFunction(MovingCameraScene):
         self.play(TransformMatchingTex(t2Int, t2Int2, key_map={"1":"1","-1":"-1"},transform_mismatches=True))
         self.play(Write(iex2))
         self.play(Write(sr))
-        self.wait(0.5)
+        self.wait(1)
         self.play(FadeOut(iex), Uncreate(sr),FadeOut(iex2))
         self.play(Write(iex3))
         self.play(t2Int2.animate.shift(LEFT*0.75),Write(alt1))
@@ -138,33 +139,150 @@ class MainFunction(MovingCameraScene):
 
         vgP1 = VGroup(alt6,t1[0],t1[4],t1[2],t2Int2, iex5, alt4)
         self.play(FadeOut(vgP1))
-        textfi = Tex("But what does this mean? Let's check it visually:")
+        textfi = Tex("But what does this all mean? Let's check it visually!")
         self.play(Write(textfi))
         self.play(textfi.animate.shift(UP*2.5))
         self.play(FadeIn(vg))
 
         l1 = ax.get_vertical_line(ax.input_to_graph_point(-1,curve), color= WHITE)
         l2 = ax.get_vertical_line(ax.input_to_graph_point(1,curve), color= WHITE)
+        l3 = ax.get_vertical_line(ax.input_to_graph_point(-2,curve), color= WHITE)
+        l4 = ax.get_vertical_line(ax.input_to_graph_point(2,curve), color= WHITE)
+        l5 = ax.get_vertical_line(ax.input_to_graph_point(-3,curve), color= WHITE)
+        l6  = ax.get_vertical_line(ax.input_to_graph_point(3,curve), color= WHITE)
 
         textfi2 = MathTex(r"\text{We've just seen that for } 1 \sigma \text{ our POP is } \int_{-1}^{1} \dfrac{e^{-\frac{x^2}{2}}}{\sqrt{2 \pi}} dx = \text{erf(}\frac{1}{\sqrt{2}}\text{)} = 0.682689... \,\approx 68\%", font_size=30).move_to(UP*2.5)
-        jk1 = Tex(r"That's just a fancy way to saying that the area bellow the curve between $\pm 1\sigma$ is $\approx 0.68$", font_size=20).move_to(DOWN*2.5)
+        jk = Tex(r"That's just a fancy way to say that the area bellow the curve between $\pm 1\sigma$ is ",r"$\approx 0.68$", font_size=30).move_to(DOWN*3)
+        jk[1].set_color_by_gradient([BLUE_D,BLUE_C,BLUE_A])
 
-        textf3 = Tex(r"But what if we want to know the POP at $2\sigma$ or $3\sigma$?\\ A really beautiful fact is that if we want to know the POP at any other SD we only have to change either the limits of integration or, an easier way is to just evaluate the error function where we want:",font_size=20)
-        textf3.move_to(DOWN*2)
+        textf3 = Tex(r"But what if we want to know the POP at $2\sigma$ or $3\sigma$?",font_size=30).move_to(UP*2.5)
+        textf4 = Tex(r"A beautiful move is just changing the integration limits to $\pm \sigma$: $\int_{-\sigma}^{\sigma} \dfrac{e^{-\frac{x^2}{2}}}{\sqrt{2 \pi}} dx$ but an easier way is to compute: ",font_size=30).move_to(DOWN*3)
+        
+        eqfin = MathTex(r"\text{erf(}\frac{\sigma}{\sqrt{2}}\text{)}").move_to(UP)
+        eqfin1 = MathTex(r"\text{erf(}\frac{1}{\sqrt{2}}\text{)} = 0.682689... \approx 68\%").move_to(DOWN*3).set_color_by_gradient([BLUE_D,BLUE_C,BLUE_A])
+        eqfin2 = MathTex(r"\text{erf(}\frac{2}{\sqrt{2}}\text{)} = 0.954499... \approx 95\%").move_to(DOWN*3).set_color_by_gradient([GREEN_D,GREEN_C,GREEN_A])
+        eqfin3 = MathTex(r"\text{erf(}\frac{3}{\sqrt{2}}\text{)} = 0.997300... \approx 99\%").move_to(DOWN*3).set_color_by_gradient([RED_D,RED_C,RED_A])
 
         area1 = ax.get_area(curve, x_range=[-1,1], fill_opacity=0.9)
         area1.set_color_by_gradient([BLUE_D,BLUE_C,BLUE_A])
+        area2 = ax.get_area(curve, x_range=[-2,2], fill_opacity=0.9)
+        area2.set_color_by_gradient([GREEN_D,GREEN_C,GREEN_A])
+        area3 = ax.get_area(curve, x_range=[-3,3], fill_opacity=0.9)
+        area3.set_color_by_gradient([RED_D,RED_C,RED_A])
+
+        fivg1 = VGroup(l1,l2,area1)
+        fivg2 = VGroup(l3,l4,area2)
+        fivg3 = VGroup(l5,l6,area3)
 
         self.play(FadeOut(textfi))
         self.play(Write(textfi2))
-        self.play(Write(l1), Write(l2))
-
-        self.play(sigma1.animate.set_color(BLUE_C), sigma2.animate.set_color(BLUE_C))
-        self.play(Write(jk1))
+        self.play(Write(jk),Write(l1), Write(l2),sigma1.animate.set_color(BLUE_C), sigma2.animate.set_color(BLUE_C))
         self.play(Create(area1), rate_func=rate_functions.smooth)
         self.play(FadeOut(textfi2))
-        self.play(Write(textf3))
+        self.play(Write(textf3), FadeOut(jk))
+        self.wait(0.5)
+        self.play(Write(textf4))
+        self.wait(0.5)
+        self.play(FadeOut(textf3), textf4.animate.shift(UP*5.5), FadeOut(vg), FadeOut(fivg1))
 
+        sr3 = SurroundingRectangle(eqfin).set_color_by_gradient([YELLOW,PINK,PURPLE])
+        self.play(Write(eqfin))
+        self.play(Create(sr3))
+        self.play(Uncreate(sr3), eqfin.animate.set_color_by_gradient([YELLOW,PINK,PURPLE]))
+        self.play(eqfin.animate.shift(DOWN*4))
+        self.wait(0.5)
+
+        self.play(TransformMatchingTex(eqfin,eqfin1,transform_mismatches=True),Write(ax),Write(sigma1),Write(sigma2),Write(sigma3),Write(sigma4),Write(sigma5),Write(sigma6),Write(curve),Write(l1), Write(l2), Create(area1),rate_func=rate_functions.smooth)
+        self.wait(1)
+        self.play(Uncreate(l1), Uncreate(l2), Uncreate(area1))
+
+        
+        self.play(sigma1.animate.set_color(WHITE),
+        sigma2.animate.set_color(WHITE),
+        sigma3.animate.set_color(GREEN_C),
+        sigma4.animate.set_color(GREEN_C),
+        TransformMatchingTex(eqfin1,eqfin2,transform_mismatches=True), 
+        Write(l3),Write(l4), Create(area2),rate_func=rate_functions.smooth)
+
+        self.wait(1)
+        self.play(Uncreate(l3), Uncreate(l4), Uncreate(area2))
+
+        self.play(sigma3.animate.set_color(WHITE),
+        sigma4.animate.set_color(WHITE),
+        sigma5.animate.set_color(RED_C),
+        sigma6.animate.set_color(RED_C),
+        TransformMatchingTex(eqfin2,eqfin3,transform_mismatches=True),Create(l5),Create(l6),Create(area3),rate_func=rate_functions.smooth)
+        self.wait()
+
+        eqfin.set_color(WHITE)
+        self.play(FadeOut(fivg3), FadeOut(textf4), FadeOut(vg))
+        self.play(TransformMatchingTex(eqfin3,eqfin,transform_mismatches=True))
+        self.play(eqfin.animate.shift(UP*3))
+        sr2 = SurroundingRectangle(eqfin).set_color_by_gradient([BLUE_C,GREEN_C])
+        self.play(Write(sr2))
+        self.wait()
+
+    
+        ax2 = Axes(
+            x_range = [0, 3, 1],
+            y_range = [0, 1, 0.5],
+            x_length = 3,
+            y_length = 1,
+            tips=False,
+            axis_config = {'include_numbers':True}
+        )
+
+        erf = ax2.plot(lambda x: 1-erfc(x), x_range=[0.01,3])
+        erf.set_color_by_gradient([YELLOW,GREEN,BLUE])
+
+        erfg = VGroup(erf,ax2).scale(2)
+
+        t = ValueTracker(0)
+
+        initial_point= [ax2.coords_to_point(t.get_value(), erf(t.get_value()))]
+
+        md = Dot(point=initial_point, radius=0.5)
+        md.add_updater(lambda x: x.move_to(ax.c2p(t.get_value(), erf(t.get_value()))))
+
+        xt = Tex(r"x = ").move_to(DOWN)
+        xt.shift(LEFT)
+        xt_value_text = always_redraw(
+            lambda: DecimalNumber(num_decimal_places=2)
+            .set_value(t.get_value())
+            .next_to(xt, RIGHT, buff=0.2)
+        )
+
+        yt = Tex(r"y = ").move_to(DOWN)
+        yt.shift(RIGHT)
+        yt_value_text = always_redraw(
+            lambda: DecimalNumber(num_decimal_places=2)
+            .set_value(erf(t.get_value()))
+            .next_to(yt, RIGHT, buff=0.2)
+        )
+
+        tef = Tex(r"To sum everything up, let's just have a quick look at the ",r"Gaussian error function",r":").move_to(UP*2.5)
+        tef[1].set_color_by_gradient([YELLOW,GREEN,BLUE])
+        self.play(Write(tef))
+        self.play(Uncreate(sr2), FadeOut(eqfin))
+        self.play(Write(ax2), Write(erf), Write(md), Write(xt), Write(yt), Write(xt_value_text), Write(yt_value_text))
+
+        self.wait(0.5)
+        self.play(t.animate.set_value(1))
+        self.wait(0.5)
+        self.play(t.animate.set_value(2))
+        self.wait(0.5)
+        self.play(t.animate.set_value(3))
+        self.wait(0.5)
+
+        self.play(Unwrite(tef), Uncreate(ax2), Uncreate(erf), Unwrite(md), Unwrite(xt), Unwrite(yt), Unwrite(xt_value_text), Unwrite(yt_value_text))
+
+        sq = Square(side_length=1, color=WHITE, fill_opacity=0.5).move_to(DOWN*0.2).scale(0.25)
+        sq.shift(RIGHT).set_color_by_gradient([PURPLE_C,WHITE])
+        qed = Tex(r'Q.E.D.').set_color_by_gradient([PURPLE_C,WHITE])
+        self.play(Write(qed), Create(sq))
+        self.wait(2)
+
+        self.play(FadeOut(sq), FadeOut(qed))
         self.wait(3)
 
 def PDF_normal(x, mu, sigma,k):
