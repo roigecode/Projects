209dbd319abcccdc7587e14efb583644a34a076b
roigecode <roigeee@gmail.com>
2022-02-12 06:06:18 +0100

    Añadidos varias fuentes de consulta

--- ethgas.py
+++ ethgas.py
@@ -9,12 +9,14 @@ class EthGasFees:
     Esta clase contiene las Gas Fees y el precio de ETH
     """
     # Constructor:
-    def __init__(self,_req,_fastest,_fast,_average,_safeLow):
+    def __init__(self,_resource,_req,_fast=0,_average=0,_safeLow=0):
+        # De momento no uso el req pero se hará:
+        self.resource = _resource
         self.request = _req
-        self.fastest = _fastest
-        self.fast = _fast
-        self.average = _average
-        self.safeLow = _safeLow
+        # Dividir entre 10, docs:
+        self.fast = float(_fast)
+        self.average = float(_average)
+        self.safeLow = float(_safeLow)
 
     # Get Eth Price:
     def get_eth_usd(self):
@@ -33,48 +35,57 @@ class EthGasFees:
     # Conversor de gwei a usd:
     def conversor_gwei_usd(self,gwei):
         ethusd = self.get_eth_usd()
-        return round(gwei/10 * ethusd/1000,2)
+        return round(gwei * ethusd/100000,2)
 
     # GETTERS:
-
-    # Fastest:
-    def get_fastest(self):
-        return self.fastest
-    
-    def get_fastest_usd(self):
-        return self.conversor_gwei_usd(self.fastest)
+    def get_resource(self):
+        return self.resource
 
     # Fast:
     def get_fast(self):
-        return self.fast
+        if self.get_resource() == "Etherscan":
+            return self.fast
+        elif self.get_resource() == "DefiPulse":
+            return self.fast/10
     
     def get_fast_usd(self):
-        return self.conversor_gwei_usd(self.fast)
-
+        if self.get_resource() == "Etherscan":
+            return self.conversor_gwei_usd(self.get_fast())
+        elif self.get_resource() == "DefiPulse":
+            return self.conversor_gwei_usd(self.get_fast())
+        
     # Average:
     def get_average(self):
-        return self.average
+        if self.get_resource() == "Etherscan":
+            return self.average
+        elif self.get_resource() == "DefiPulse":
+            return self.average/10
     
     def get_average_usd(self):
-        return self.conversor_gwei_usd(self.average)
+        return self.conversor_gwei_usd(self.get_average())
 
     # Safe Low:
     def get_safeLow(self):
-        return self.safeLow
+        if self.get_resource() == "Etherscan":
+            return self.safeLow
+        elif self.get_resource() == "DefiPulse":
+            return self.safeLow/10
     
     def get_safeLow_usd(self):
-        return self.conversor_gwei_usd(self.safeLow)
+        return self.conversor_gwei_usd(self.get_safeLow())
 
     # Printers:
     def display_gwei(self):
-        return "\nGWEI >> Fastest: {}  Fast: {}  Average: {}  Safe Low: {}".format(self.get_fastest(), 
+        return "\n{} || GWEI >> Fast: {}  Average: {}  Safe Low: {}".format(
+                                                                                   self.get_resource(),
                                                                                    self.get_fast(),
                                                                                    self.get_average(), 
                                                                                    self.get_safeLow())
 
     # 1 ether son 1,000,000,000 Gwei
     def display_usd(self):
-        return "\nUDS >> Fastest: {}$  Fast: {}$  Average: {}$  Safe Low: {}$".format(self.get_fastest_usd(),
+        return "{} || UDS >> Fast: {}$  Average: {}$  Safe Low: {}$".format(
+                                                                           self.get_resource(),
                                                                            self.get_fast_usd(),
                                                                            self.get_average_usd(),
                                                                            self.get_safeLow_usd())
@@ -87,19 +98,34 @@ if __name__ == "__main__":
     # If we get <200> = Success, <404> Not Found, else Generic Error:
     try:
         req = requests.get("https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key={}".format(config.API_KEY))
+        req_etherscan = requests.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={}".format(config.API_ETHERSCAN))
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
+
+    # DEFI-PULSE:
 
     # Convertimos los datos en json:
     json_resp = req.json()
 
     # Cogemos los datos:
-    fastest = json_resp['fastest']
-    fast = json_resp['fast']
-    average = json_resp['average']
-    safeLow = json_resp['safeLow']
+    fast_defipulse = json_resp['fast']
+    average_defipulse = json_resp['average']
+    safeLow_defipulse = json_resp['safeLow']
 
-    eth_object = EthGasFees(req,fastest,fast,average,safeLow)
+    eth_defipulse = EthGasFees("DefiPulse",req,fast_defipulse,average_defipulse,safeLow_defipulse)
 
-    print(eth_object.display_gwei())
-    print(eth_object.display_usd())
+    print(eth_defipulse.display_gwei())
+    print(eth_defipulse.display_usd())
+
+
+    # ETHERSCAN:
+    json_resp2 = req_etherscan.json()
+
+    fast_etherscan = json_resp2['result']['FastGasPrice']
+    average_etherscan = json_resp2['result']['ProposeGasPrice']
+    safeLow_etherscan = json_resp2['result']['suggestBaseFee']
+
+    eth_etherscan = EthGasFees("Etherscan",req,fast_etherscan,average_etherscan,safeLow_etherscan)
+
+    print(eth_etherscan.display_gwei())
+    print(eth_etherscan.display_usd())
